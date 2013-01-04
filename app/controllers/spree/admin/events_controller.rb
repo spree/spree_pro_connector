bb31f6ab463b65ef2d4d8d9669b04185cdf6d360
John Dyer <lbrapid@gmail.com>
2013-01-04 13:13:34 -0500

    Added events controller

---
+++ app/controllers/spree/admin/events_controller.rb
@@ -0,0 +1,7 @@
+module Spree
+  module Admin
+    class EventsController < Spree::Admin::BaseController
+      respond_to :html
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Spree::Core::Engine.routes.draw do
-  # Add your extension routes here
+  namespace :admin do
+    resources :orders do
+      resources :events, :only => :index
+    end
+  end
 end
