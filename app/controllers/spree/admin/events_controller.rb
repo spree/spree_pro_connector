c9867a9e449b0b83dd4d16124e058ced7dfb4eb9
John Dyer <lbrapid@gmail.com>
2013-01-04 13:42:56 -0500

    Basic iframe rendering of events on 'Order Events' tab. NOTE: Auth token is hardcoded

--- app/controllers/spree/admin/events_controller.rb
+++ app/controllers/spree/admin/events_controller.rb
@@ -2,6 +2,10 @@ module Spree
   module Admin
     class EventsController < Spree::Admin::BaseController
       respond_to :html
+
+      def index
+        @order = Order.find_by_number!(params[:order_id])
+      end
     end
   end
 end
