022bc01fb857c018cfba54825d8bac10181355fe
John Dyer <lbrapid@gmail.com>
2013-01-04 13:53:32 -0500

    Use exclamation sign icon for order events tab

--- app/overrides/add_events_to_order_tabs.rb
+++ app/overrides/add_events_to_order_tabs.rb
@@ -3,7 +3,7 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
                      :insert_bottom => "[data-hook='admin_order_tabs']",
                      :text => %q{
                        <li<%== ' class="active"' if current == 'Events' %>>
-                         <%= link_to_with_icon 'icon-cogs', t(:order_events), spree.admin_order_events_url(@order) %>
+                         <%= link_to_with_icon 'icon-exclamation-sign', t(:order_events), spree.admin_order_events_url(@order) %>
                        </li>
                      },
                      :disabled => false)
