e64275bc9adb6af96055373fe90e6251e7b1b406
John Dyer <lbrapid@gmail.com>
2013-01-04 13:13:14 -0500

    Override to add events tab to order tabs

---
+++ app/overrides/add_events_to_order_tabs.rb
@@ -0,0 +1,9 @@
+Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
+                     :name => "add_events_to_order_tabs",
+                     :insert_bottom => "[data-hook='admin_order_tabs']",
+                     :text => %q{
+                       <li<%== ' class="active"' if current == 'Events' %>>
+                         <%= link_to_with_icon 'icon-cogs', t(:order_events), spree.admin_order_events_url(@order) %>
+                       </li>
+                     },
+                     :disabled => false)
