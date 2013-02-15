3d6f73c3dcd9e7c07e8593b6633759b5c0dbb754
Andrew Hooker <geekoncoffee@gmail.com>
2013-02-15 10:22:15 -0600

    Use different augury based on environment

--- app/controllers/spree/admin/events_controller.rb
+++ app/controllers/spree/admin/events_controller.rb
@@ -5,6 +5,14 @@ module Spree
 
       def index
         @order = Order.find_by_number!(params[:order_id])
+	@integration_url = case Rails.env
+	  when 'development'
+	    "http://augury-admin.dev"
+	  when 'staging'
+            "http://aug-stg1.spree.mx"
+	  when 'production'
+            "http://aug-app1.spree.mx"
+	  end
       end
     end
   end
