8b5ce6bb22cfa9331a3a2ada131d5a290ab64d5d
John Dyer <lbrapid@gmail.com>
2013-06-03 11:08:45 -0400

    Use augury admin api for integration registration

--- app/controllers/spree/admin/integration_controller.rb
+++ app/controllers/spree/admin/integration_controller.rb
@@ -1,8 +1,10 @@
 module Spree
   module Admin
     class IntegrationController < Spree::Admin::BaseController
-      def register
+      include HTTParty
+      base_uri 'http://augury-admin.dev/api'
 
+      def register
         email = 'integrator@spreecommerce.com'
         if user = Spree::User.where('email' => email).first
           # do nothing, for now....
@@ -16,13 +18,26 @@ module Spree
           user.generate_spree_api_key!
         end
 
-        # HTTParty.post to see if store is already registered (and get store_if so)
-        #
-        # if not another post to register
-        #
+        # Register Store if Spree::Config[:store_id] isn't present
+        unless Spree::Config[:store_id].present?
+          options = {
+            body: {
+              signup: {
+                name: Spree::Config[:site_name],
+                url: request.url,
+                version: Spree.version,
+                api_key: user.spree_api_key,
+                email: email,
+              }
+            }
+          }
 
-        Spree::Config[:store_id] = '123' #should be real store_id returned either post above.
-
+          response = self.class.post('/signups.json', options)
+          if response.code == 201
+            Spree::Config[:store_id] = response["store_id"]
+            Spree::Config[:pro_api_key] = response["auth_token"]
+          end
+        end
 
         redirect_to :action => :show
       end
