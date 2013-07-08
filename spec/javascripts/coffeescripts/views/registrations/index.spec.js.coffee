eb22bdafa4dc64b230330db92b067a155ab41e6b
John Dyer <lbrapid@gmail.com>
2013-07-08 11:00:13 -0400

    Registration index view spec

--- spec/javascripts/coffeescripts/views/registrations/index.spec.js.coffee
+++ spec/javascripts/coffeescripts/views/registrations/index.spec.js.coffee
@@ -1,13 +1,14 @@
 describe "Registrations Index View", ->
   beforeEach ->
     $('body').append('<div id="integration-main"></div>')
-    @registration = new Backbone.Model
+    @registration = new Augury.Models.Registration
       name: "Foobar"
       url: "http://google.com"
       keys: ["order:completed"]
-    @collection = new Backbone.Collection [@registration]
+    @collection = new Augury.Collections.Registrations [@registration]
     @view = new Augury.Views.Registrations.Index(collection: @collection)
     @viewRenderSpy = sinon.spy(@view, "render")
+    sinon.stub(Augury, 'handle_link_clicks')
     $('#integration-main').html(@view.render().el)
 
   afterEach ->
@@ -22,3 +23,11 @@ describe "Registrations Index View", ->
     expect($('tbody td:first')).toHaveText("Foobar")
     expect($('tbody td:nth-child(2)')).toHaveText("http://google.com")
     expect($('tbody td:nth-child(3)')).toHaveText("order:completed")
+
+  describe "clicking the new registration link", ->
+    beforeEach ->
+      @routerSpy = sinon.spy(Augury.Routers.Registrations.prototype, 'new')
+      $('a.new').trigger('click')
+
+    it "shows the new registration form", ->
+      expect($('fieldset')).toBeVisible()
--- spec/javascripts/compiled/views/registrations/index.spec.js
+++ spec/javascripts/compiled/views/registrations/index.spec.js
@@ -2,16 +2,17 @@
   describe("Registrations Index View", function() {
     beforeEach(function() {
       $('body').append('<div id="integration-main"></div>');
-      this.registration = new Backbone.Model({
+      this.registration = new Augury.Models.Registration({
         name: "Foobar",
         url: "http://google.com",
         keys: ["order:completed"]
       });
-      this.collection = new Backbone.Collection([this.registration]);
+      this.collection = new Augury.Collections.Registrations([this.registration]);
       this.view = new Augury.Views.Registrations.Index({
         collection: this.collection
       });
       this.viewRenderSpy = sinon.spy(this.view, "render");
+      sinon.stub(Augury, 'handle_link_clicks');
       return $('#integration-main').html(this.view.render().el);
     });
     afterEach(function() {
@@ -22,10 +23,19 @@
       expect($('table.index')).toBeVisible();
       return expect($('a.new')).toBeVisible();
     });
-    return it("has the correct parameter content", function() {
+    it("has the correct parameter content", function() {
       expect($('tbody td:first')).toHaveText("Foobar");
       expect($('tbody td:nth-child(2)')).toHaveText("http://google.com");
       return expect($('tbody td:nth-child(3)')).toHaveText("order:completed");
+    });
+    return describe("clicking the new registration link", function() {
+      beforeEach(function() {
+        this.routerSpy = sinon.spy(Augury.Routers.Registrations.prototype, 'new');
+        return $('a.new').trigger('click');
+      });
+      return it("shows the new registration form", function() {
+        return expect($('fieldset')).toBeVisible();
+      });
     });
   });
 
