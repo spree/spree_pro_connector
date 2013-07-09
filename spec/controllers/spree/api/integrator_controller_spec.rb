a687cd6278ece4b88153fa231e68f638733ce186
cmar <christopher.mar@gmail.com>
2013-07-09 08:24:43 -0400

    order orders by updated_at for integration controllers

--- spec/controllers/spree/api/integrator_controller_spec.rb
+++ spec/controllers/spree/api/integrator_controller_spec.rb
@@ -5,11 +5,16 @@ module Spree
     render_views
 
     before do
+      controller.stub(:requires_authentication? => false)
       stub_authentication!
     end
 
     it 'gets everything changed since' do
-      order = create(:order,
+      order1 = create(:order,
+             :updated_at => 1.days.ago,
+             :completed_at => 1.days.ago)
+
+      order2 = create(:order,
              :updated_at => 2.days.ago,
              :completed_at => 2.days.ago)
 
@@ -17,11 +22,10 @@ module Spree
                order_page: 1,
                order_per_page: 1
 
-
-      json_response['orders']['count'].should eq 1
+      json_response['orders']['count'].should eq 2
       json_response['orders']['current_page'].should eq 1
 
-      json_response['orders']['page'].first['number'].should eq order.number
+      json_response['orders']['page'].first['number'].should eq order2.number
       json_response['orders']['page'].first.should have_key('ship_address')
       json_response['orders']['page'].first.should have_key('bill_address')
       json_response['orders']['page'].first.should have_key('payments')
