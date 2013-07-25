e53f2000310a10dde423003621e60cdf69d0935e
Washington Luiz <huoxito@gmail.com>
2013-07-25 13:55:14 -0300

    Add ship_total and tax_total to order_attributes

--- app/controllers/spree/api/v1/orders_controller_decorator.rb
+++ app/controllers/spree/api/v1/orders_controller_decorator.rb
@@ -8,6 +8,24 @@ Spree::Api::V1::OrdersController.class_eval do
   end
 
   def order_attributes
-    [:id, :number, :item_total, :total, :state, :adjustment_total, :user_id, :created_at, :updated_at, :completed_at, :payment_total, :shipment_state, :payment_state, :email, :special_instructions, :total_weight, :locked_at]
+    [:id,
+     :number,
+     :item_total,
+     :total,
+     :state,
+     :adjustment_total,
+     :user_id,
+     :created_at,
+     :updated_at,
+     :completed_at,
+     :payment_total,
+     :shipment_state,
+     :payment_state,
+     :email,
+     :special_instructions,
+     :ship_total,
+     :tax_total,
+     :total_weight,
+     :locked_at]
   end
 end
